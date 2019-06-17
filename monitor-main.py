e7895eb65a518611c5162bce1c3ad3b44572cbf5
DwayneT86 <dwaynetruex@yahoo.com>
2019-06-17 01:29:48 +0100

    edited monitor-main.py

--- monitor-main.py
+++ monitor-main.py
@@ -1,1 +1,23 @@
-#Main program
+#!/usr/bin/env python
+
+#Written by Dwayne Truex June 2019
+#Written for PiZeroW
+#Contact: dwaynetruex@yahoo.com
+
+# This program was designed to control multiple relays
+# and read multiple inputs
+
+
+
+#import needed modules
+
+import RPi.GPIO as GPIO #  Import GPIO Module
+from time import sleep # Import sleep Module for timing
+
+# Setup GPIO Pins 
+
+GPIO.setmode(GPIO.BCM)  # Configures how we are describing our pin numbering
+GPIO.setwarnings(False)  # Disable Warnings
+
+
+
