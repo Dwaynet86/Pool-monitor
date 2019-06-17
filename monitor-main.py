a6473bd201f06dfb963e6061e4232e38a5c6db5e
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-16 20:24:05 -0500

    Update monitor-main.py

--- monitor-main.py
+++ monitor-main.py
@@ -20,4 +20,15 @@ GPIO.setmode(GPIO.BCM)  # Configures how we are describing our pin numbering
 GPIO.setwarnings(False)  # Disable Warnings
 
 
+def create_database():
 
+    conn = cymysql.connect(servername, username, password)
+    curs = conn.cursor()
+    curs.execute("SET sql_notes = 0; ")  # Hide Warnings
+
+    curs.execute("CREATE DATABASE IF NOT EXISTS {}".format(dbname))
+
+    curs.execute("SET sql_notes = 1; ")  # Show Warnings
+    conn.commit()
+    conn.close()
+    return
